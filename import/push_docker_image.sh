41ae331130e1df9075cbdd9fbcf9ce1d51dba7ce
AfikAT <52320907+AfikAT@users.noreply.github.com>
2021-06-20 08:59:59 +0300

    Update push_docker_image.sh

--- import/push_docker_image.sh
+++ import/push_docker_image.sh
@@ -4,10 +4,10 @@ build=${1}
 registry_path=${2}
 
 ##Building the image and push the image to dockerhub##
-docker build . ${registry_path}:latest;docker push ${registry_path}:latest
+docker build . -t ${registry_path}:latest;docker push ${registry_path}:latest
 
 
 
 ##Pushing image with BuildNumber##
-docker build . ${registry_path}:${build};docker push ${registry_path}:${build}
+docker build . -t ${registry_path}:${build};docker push ${registry_path}:${build}
 
