23d7ff1bf5e407c542d05f3c24f65eff45e006d8
AfikAT <52320907+AfikAT@users.noreply.github.com>
2021-06-20 08:48:09 +0300

    Update push_docker_image.sh

--- import/push_docker_image.sh
+++ import/push_docker_image.sh
@@ -2,12 +2,13 @@
 
 build=${1}
 registry_path=${2}
-
+echo ${build) echo ${registry_path}
+ls -l
 ##Building the image using latest tag & push to dockerhub
 
-docker build . ${registry_path}:latest && docker push ${registry_path}:latest
+##docker build . ${registry_path}:latest && docker push ${registry_path}:latest
 
 ##Pushing image with BuildNumber##
 
-docker build . ${registry_path}:${build} && docker push ${registry_path}:${build}
+##docker build . ${registry_path}:${build} && docker push ${registry_path}:${build}
 
