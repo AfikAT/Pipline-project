3d7140f0b440adb69dd31322307bb497e865e2fe
AfikAT <52320907+AfikAT@users.noreply.github.com>
2021-06-20 10:37:16 +0300

    Update push_docker_image.sh

--- import/push_docker_image.sh
+++ import/push_docker_image.sh
@@ -2,11 +2,9 @@
 
 build=${1}
 registry_path=${2}
-USERNAME=${3}
-PASSWORD=${4}
-docker login -u "${USERNAME}" --password-stdin "${PASSWORD}"
 ##Building the image and push the image to dockerhub##
 #docker build --network sonarnet -t ${registry_path}:latest .;docker push ${registry_path}:latest
+docker push ${registry_path}:latest
 
 
 
